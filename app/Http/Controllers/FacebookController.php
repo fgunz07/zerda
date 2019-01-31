f54103d2e8f63ef3ea0a134a34969d43e67793e2
Rey Marky <mhikeking0@gmail.com>
2019-01-31 22:22:23 +0800

    Updated login using facebook now working

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -4,21 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
+use App\Services\SocialFacebookAccountService;
 
 class FacebookController extends Controller
 {
 
-    public function getAuthFacebook() {
-
-    	// return facebook driver to redirect user to facebook login/confirmation
+	/**
+   * Create a redirect method to facebook api.
+   *
+   * @return void
+   */
+    public function redirect()
+    {
         return Socialite::driver('facebook')->redirect();
-
     }
 
-    public function fbOauth() {
+     /**
+     * Return a callback method from facebook api.
+     *
+     * @return callback URL from facebook
+     */
+    public function callback(SocialFacebookAccountService $service)
+    {
 
-    	dd(Socialite::driver('facebook')->user());
+    	$fields = Socialite::driver('facebook')
+    						->fields([
+			                    'name', 
+			                    'first_name', 
+			                    'last_name',
+			                    'middle_name',
+			                    'email',
+			                    'gender', 
+			                    'verified'
+			                ]);
 
+        $user = $service->createOrGetUser($fields->user());
 
+        auth()->login($user);
+
+        return redirect()->to('/home');
     }
 }
